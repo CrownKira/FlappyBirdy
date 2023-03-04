f3433ef344fd79094fe48e621609bba514a91095
Kar Wi <t.karwi@yahoo.com>
2023-03-05 02:11:58 +0800

    fix: Default playerScore

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -8,34 +8,31 @@ public class BirdScript : MonoBehaviour
   // public to expose to other classes; eg. the "ui interface" can then access these and do something to it;
   public Rigidbody2D myRigidbody;
   public float flapStrength;
-
-
+  public LogicScript logic;
+  public bool birdIsAlive = true;
 
   // run precisely once 
   // Start is called before the first frame update
   void Start()
   {
-    gameObject.name = "Bob birdington";
+    // gameObject.name = "Bob birdington";
+    logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
   }
 
   // every single frame 
   // Update is called once per frame
   void Update()
   {
-    if (Input.GetKeyDown(KeyCode.Space) == true)
+
+    if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
     {
       myRigidbody.velocity = Vector2.up * flapStrength;
     }
   }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    logic.gameOver();
+    birdIsAlive = false;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
   public int playerScore;
   public Text scoreText;
+  public GameObject gameOverScreen;
 
   // make the function more versatile; keep it open for future ideas; make it less rigid;
   [ContextMenu("Increase Score")]
@@ -14,6 +16,19 @@ public class LogicScript : MonoBehaviour
   {
     playerScore += scoreToAdd;
     scoreText.text = playerScore.ToString();
+  }
+
+  public void restartGame()
+  {
+    // look for name of the scene
+    // here can just put current scene
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+  }
+
+  public void gameOver()
+  {
+    gameOverScreen.SetActive(true);
   }
 
 }
