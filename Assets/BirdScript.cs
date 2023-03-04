223fb2937a7216f3bafd22cddae7c4aa289d235e
Kar Wi <t.karwi@yahoo.com>
2023-03-04 23:05:26 +0800

    chore: Add gitignore

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+  public Rigidbody2D myRigidbody;
+
+  // run precisely once 
+  // Start is called before the first frame update
+  void Start()
+  {
+    gameObject.name = "Bob birdington";
+  }
+
+  // every single frame 
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
